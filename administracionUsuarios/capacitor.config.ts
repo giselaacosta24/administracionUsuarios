34da1fc763b387c266171882d9c77abe10614f36
giselaacosta24 <giseacosta651@gmail.com>
2022-05-07 09:12:11 -0300

    .

---
+++ administracionUsuarios/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.administracion.starter',
+  appName: 'administracionUsuarios',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
